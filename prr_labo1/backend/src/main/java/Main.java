07cd4e06f2e9689862d600f7b8ff888f9fccc861
Bertral <antoine.friant@gmail.com>
2017-11-08 15:27:25 +0100

    init

---
+++ prr_labo1/backend/src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Project : prr_labo1
+ * Date : 08.11.17
+ */
+public class Main {
+
+    // Le backend implémente Lamport
+}
---
+++ prr_labo1/frontend/src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Project : prr_labo1
+ * Date : 08.11.17
+ */
+public class Main {
+
+    // Le frontend tourne sur la VM qui servira d'interface au backend
+}
