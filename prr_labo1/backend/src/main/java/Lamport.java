630c766a092f998bc23324a02e3b045a28153c17
Bertral <antoine.friant@gmail.com>
2017-11-16 10:43:34 +0100

    push

---
+++ prr_labo1/backend/src/main/java/Lamport.java
@@ -0,0 +1,16 @@
+/**
+ * Project : prr_labo1
+ * Author(s) : Antoine Friant
+ * Date : 16.11.17
+ */
+public class Lamport implements Runnable {
+
+    public Lamport() {
+
+    }
+
+    public void run() {
+
+    }
+
+}
