00a3c2102ba3508aa2ac40ee940fb2fad841a806
Bertral <antoine.friant@gmail.com>
2017-12-04 15:58:40 +0100

    refactor + support multisite + préparation remote

--- prr_labo1/backend/src/main/java/Lamport.java
+++ prr_labo1/backend/src/main/java/Lamport.java
@@ -3,36 +3,35 @@ import java.util.List;
 
 /**
  * Project : prr_labo1
- * Author(s) : Antoine Friant
  * Date : 16.11.17
  */
-enum TYPE{REQUETE,QUITTANCE, LIBERE}
 
-class Request{
-    TYPE type;
-    long estampille;
-    int originSite;
+public class Lamport implements Runnable {
+    enum TYPE {REQUETE, QUITTANCE, LIBERE}
 
-    public Request(TYPE type, long estampille, int originSite){
-        this.estampille = estampille;
-        this.type = type;
-        this.originSite = originSite;
+    class Request {
+        TYPE type;
+        long estampille;
+        int originSite;
+
+        public Request(TYPE type, long estampille, int originSite) {
+            this.estampille = estampille;
+            this.type = type;
+            this.originSite = originSite;
+        }
+
+        public long getEstampille() {
+            return estampille;
+        }
+
+        public int getOriginSite() {
+            return originSite;
+        }
+
+        public TYPE getType() {
+            return type;
+        }
     }
-
-    public long getEstampille() {
-        return estampille;
-    }
-
-    public int getOriginSite() {
-        return originSite;
-    }
-
-    public TYPE getType() {
-        return type;
-    }
-}
-
-public class Lamport  implements Runnable {
 
     private int numSite;
     private int nbSite;
@@ -43,7 +42,7 @@ public class Lamport  implements Runnable {
 
     public Lamport(int numSite, int nbSite) {
         this.numSite = numSite;
-        this.nbSite= nbSite;
+        this.nbSite = nbSite;
         this.clockLogical = 0;
         this.scAccorde = false;
         this.tFile = new ArrayList(nbSite);
@@ -52,15 +51,15 @@ public class Lamport  implements Runnable {
         initFile();
     }
 
-    private void initTFile(){
-        for(int i = 0; i < tFile.size(); i++){
+    private void initTFile() {
+        for (int i = 0; i < tFile.size(); i++) {
             tFile.add(i, new Request(TYPE.LIBERE, 0, i));
         }
     }
 
-    private void initFile(){
-        for(int i = 0; i < file.size(); i++){
-            file.add(i,i);
+    private void initFile() {
+        for (int i = 0; i < file.size(); i++) {
+            file.add(i, i);
         }
     }
 
@@ -68,63 +67,63 @@ public class Lamport  implements Runnable {
         // Gestion des 4 rendez-vous
     }
 
-    public boolean permission(int me){
+    public boolean permission(int me) {
         boolean accord = true;
-        for(int i = 0; i < file.size(); i++){
-            if(i != me){
-                accord = (tFile.get(me).getEstampille() < tFile.get(i).getEstampille() ) ||
-                        ( tFile.get(me).getEstampille() == tFile.get(i).getEstampille() && me < i) ;
+        for (int i = 0; i < file.size(); i++) {
+            if (i != me) {
+                accord = (tFile.get(me).getEstampille() < tFile.get(i).getEstampille()) ||
+                        (tFile.get(me).getEstampille() == tFile.get(i).getEstampille() && me < i);
             }
         }
         return accord;
     }
 
-    public void envoi(Request req, int dest){
+    public void envoi(Request req, int dest) {
         // Envoyer au site dest, la requete et mon num de site
     }
 
-    public void demande(){
+    public void demande() {
         // Maj horloge interne
         this.clockLogical += 1;
         // Enregistre la requête dans sa liste
         Request req = new Request(TYPE.REQUETE, clockLogical, numSite);
         tFile.add(this.numSite, req);
         // Signaler à tous les autres sites la nouvelle requête
-        for(int i = 0; i < file.size(); i++){
-            if(i != numSite){
+        for (int i = 0; i < file.size(); i++) {
+            if (i != numSite) {
                 envoi(req, i);
             }
         }
         scAccorde = permission(numSite);
     }
 
-    public void fin(){
+    public void fin() {
         // Enregistre la requête dans sa liste
         Request req = new Request(TYPE.LIBERE, clockLogical, numSite);
         tFile.add(this.numSite, req);
         // Signaler à tous les autres sites la nouvelle requête
-        for(int i = 0; i < file.size(); i ++){
-            if(i != numSite){
+        for (int i = 0; i < file.size(); i++) {
+            if (i != numSite) {
                 envoi(req, i);
             }
         }
         scAccorde = false;
     }
 
-    public void recoit(Request req){
+    public void recoit(Request req) {
         // Maj de l'horloge logique
-        clockLogical = Math.max(clockLogical, req.getEstampille())+1;
-        switch (req.getType()){
+        clockLogical = Math.max(clockLogical, req.getEstampille()) + 1;
+        switch (req.getType()) {
             case REQUETE:
                 tFile.add(req.getOriginSite(), req);
-                envoi(new Request(TYPE.QUITTANCE,clockLogical,numSite),req.getOriginSite());
-                 break;
+                envoi(new Request(TYPE.QUITTANCE, clockLogical, numSite), req.getOriginSite());
+                break;
             case LIBERE:
                 tFile.add(req.getOriginSite(), req);
                 break;
             case QUITTANCE:
-                if(tFile.get(req.originSite).getType() != TYPE.REQUETE){
-                    tFile.add(req.getOriginSite(),req);
+                if (tFile.get(req.originSite).getType() != TYPE.REQUETE) {
+                    tFile.add(req.getOriginSite(), req);
                 }
                 break;
         }
--- prr_labo1/frontend/src/main/java/Main.java
+++ prr_labo1/frontend/src/main/java/Main.java
@@ -9,15 +9,15 @@ public class Main {
     // Le frontend tourne sur la VM qui servira d'interface au backend
 
     public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("Argument invalide : [numéro du site]");
+        }
 
         // connexion au serveur RMI
         try {
-            Remote r = Naming.lookup("rmi://localhost/Data");
+            Remote r = Naming.lookup("rmi://localhost/Data"+args[0]);
             Data data = (Data)r;
 
-            System.out.println("Received data : " + data.getValue());
-            System.out.println("Received data : " + data.getValue());
-            System.out.println("Received data : " + data.getValue());
             System.out.println("Received data : " + data.getValue());
 
 
