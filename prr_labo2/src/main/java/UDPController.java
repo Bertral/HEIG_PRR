0e8c8da48e3ec4a4d9e82ed2d65868b9c711d2ac
Bertral <antoine.friant@gmail.com>
2017-12-14 14:49:01 +0100

    UDP fini

--- prr_labo2/src/main/java/UDPController.java
+++ prr_labo2/src/main/java/UDPController.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.net.*;
-import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Properties;
 
@@ -9,16 +8,41 @@ import java.util.Properties;
  * Date : 14.12.17
  */
 public class UDPController {
-    public static final byte MESSAGE_ANNOUNCE = 0;
-    public static final byte MESSAGE_RESULT = 1;
+    public enum MessageType {
+        MESSAGE_ANNOUNCE((byte) 0),
+        MESSAGE_RESULT((byte) 1);
+
+        private byte value;
+
+        MessageType(byte b) {
+            value = b;
+        }
+
+        public byte getByte() {
+            return value;
+        }
+    }
+
+    public class Message {
+        private MessageType messageType;
+        private byte site;
+        private int aptitude;
+
+        public Message(MessageType messageType, byte site, int aptitude) {
+            this.messageType = messageType;
+            this.site = site;
+            this.aptitude = aptitude;
+        }
+    }
 
     private DatagramSocket socket;
     private HashMap<Byte, InetSocketAddress> network;
 
-    public UDPController() {
+    public UDPController(byte siteId) {
         // TODO : remplir network à partir d'un fichier (comme labo précédent)
         // récupération de la liste des serveurs
         Properties properties = new Properties();
+        network = new HashMap<>();
         try {
             properties.load(Main.class.getClassLoader().getResourceAsStream("sites.properties"));
         } catch (IOException e) {
@@ -27,11 +51,12 @@ public class UDPController {
         }
 
         for (String name : properties.stringPropertyNames()) {
-
+            String[] address = properties.getProperty(name).split(":");
+            network.put(Byte.parseByte(name), new InetSocketAddress(address[0], Integer.parseInt(address[1])));
         }
 
         try {
-            socket = new DatagramSocket();
+            socket = new DatagramSocket(network.get(siteId).getPort());
         } catch (SocketException e) {
             e.printStackTrace();
             return;
@@ -43,7 +68,7 @@ public class UDPController {
     }
 
     public void sendResult(byte destination, byte electedSite) {
-        byte[] array = {MESSAGE_RESULT, electedSite};
+        byte[] array = {MessageType.MESSAGE_RESULT.getByte(), electedSite};
 
         try {
             socket.send(new DatagramPacket(array, array.length, network.get(destination)));
@@ -53,12 +78,35 @@ public class UDPController {
     }
 
     public void sendAnnounce(byte destination, byte bestSite, int aptitude) {
-        byte[] array = {MESSAGE_ANNOUNCE, bestSite, (byte) ((aptitude >> 8) & 0xFF), (byte) (aptitude & 0xFF)};
+        byte[] array = {MessageType.MESSAGE_ANNOUNCE.getByte(), bestSite, (byte) (aptitude >> 24), (byte) (aptitude
+                >> 16), (byte) (aptitude >> 8), (byte) aptitude};
 
         try {
             socket.send(new DatagramPacket(array, array.length, network.get(destination)));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public Message listen() {
+        DatagramPacket packet = new DatagramPacket(new byte[6], 6);
+
+        try {
+            socket.receive(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        byte[] data = packet.getData();
+
+        MessageType type = null;
+        if (data[0] == MessageType.MESSAGE_ANNOUNCE.getByte()) {
+            type = MessageType.MESSAGE_ANNOUNCE;
+        } else if (data[1] == MessageType.MESSAGE_RESULT.getByte()) {
+            type = MessageType.MESSAGE_RESULT;
+        }
+
+        return new Message(type, data[1],
+                data[2] << 24 | (data[3] & 0xFF) << 16 | (data[4] & 0xFF) << 8 | (data[5] & 0xFF));
     }
 }
