73a1615334efe870872cc6df98486626d5c625db
Michela Zucca <michela.zucca@heig-vd.ch>
2018-01-17 12:31:37 +0100

    avant pull

--- prr_labo4/src/main/java/Terminaison.java
+++ prr_labo4/src/main/java/Terminaison.java
@@ -3,7 +3,39 @@ public class Terminaison {
 
     private Message T_message;
     private T_Etat etat ;
-    public Terminaison(){
+    private int moi;
+    private int N;
+    private UDPController application;
+
+    public Terminaison(UDPController application, int proc, int N){
         this.etat  = T_Etat.actif;
+        this.moi = proc;
+        this.application = application;
+        this.N = N;
+    }
+
+    public void travail(Message msg){
+        switch(msg.getMessageType()){
+            case REQUETE:
+                // TODO faire le travail demandé
+                break;
+            case JETON:
+                if(moi == 1 && etat == T_Etat.inactif){
+                    application.send(2, new Message(Message.MessageType.FIN));
+                }else{
+                    // TODO attendre que le travaille soit terminé
+                    etat= T_Etat.inactif;
+                    application.send( ((moi % N) + 1), new Message(Message.MessageType.JETON));
+                }
+                break;
+            case FIN:
+                if(moi == 1){
+                    // TODO terminer
+                }
+                else{
+                    application.send(((moi % N) + 1), new Message(Message.MessageType.FIN));
+                }
+                break;
+        }
     }
 }
